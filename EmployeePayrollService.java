2d537c8e36a6d41be1d62396dc46233d89444d86
KratiGupta <kratigupta8545@gmail.com>
2021-08-23 11:42:20 +0530

    Added Welcome message Successfully

---
+++ EmployeePayrollService.java
@@ -0,0 +1,12 @@
+package employeepayrollservice;
+
+public class EmployeePayrollService {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Welcome to master branch of Employee Payroll Service");
+		
+	}
+
+}
+		
