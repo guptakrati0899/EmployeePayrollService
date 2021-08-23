58097e83e96f1f240e5d909813a7a07df356ab81
KratiGupta <kratigupta8545@gmail.com>
2021-08-23 11:47:43 +0530

    Added the code for taking input employee payroll data from console

--- EmployeePayrollService.java
+++ EmployeePayrollService.java
@@ -1,12 +1,41 @@
 package employeepayrollservice;
 
-public class EmployeePayrollService {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class EmployeePayrollService {
+	public enum IOService {CoONSOLE_IO, File_IO, DB_IO, REST_IO}
+	private List<EmployeePayrollData> employeePayrollList;
+	
+	public EmployeePayrollService() {}
+	public EmployeePayrollService(List<EmployeePayrollData>employeePayrollList){
+		this.employeePayrollList = employeePayrollList;
+	}
 	
 	public static void main(String[] args) {
-		System.out.println("Welcome to master branch of Employee Payroll Service");
+		List<EmployeePayrollData>employeePayrollList = new ArrayList<EmployeePayrollData>();
+		EmployeePayrollService employeePayrollService = new EmployeePayrollService(employeePayrollList);
+		Scanner consoleInputReader = new Scanner(System.in);
+		employeePayrollService.readEmployeePayrollData(consoleInputReader);
+		employeePayrollService.writeEmployeePayrollData();
 		
+	
+	}
+	
+	private void readEmployeePayrollData(Scanner consoleInputReader) {
+		System.out.println("Enter Employee Id");
+		int id = consoleInputReader.nextInt();
+		System.out.println("Enter Employee Name");
+		String name = consoleInputReader.next();
+		System.out.println("Enter Employee Salary");
+		double salary = consoleInputReader.nextDouble();
+		employeePayrollList.add(new EmployeePayrollData(id,name,salary));	
+		
+	}
+	
+	private void writeEmployeePayrollData() {
+		System.out.println("\nWriting Employee Paroll Roaster to Console\n" + employeePayrollList);
 	}
 
 }
-		
